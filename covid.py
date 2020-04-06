737f255f21b8c1594b1503efe3e4e64c2361d01b
Md Hassan <mohammada@metroguild.com>
2020-04-06 17:23:59 +0530

    Readibility Modification

--- covid.py
+++ covid.py
@@ -6,22 +6,23 @@ from flask import request
 
 
 app = Flask(__name__)
+
+
 @app.route("/")
 def get_secret_message():
     return "Wassup"
+
 
 @app.route("/covid")
 def get_data():
     state = request.args.get('state', default='', type=str)
     district = request.args.get('district', default='', type=str)
-
+    district_data = []
     india_data = requests.get('https://api.covid19india.org/v2/state_district_wise.json').json()
-    for state_data in india_data:
-        if state.lower() in state_data['state'].lower():
-            for district_data in state_data['districtData']:
-                if district.lower() in district_data['district'].lower():
-                    return district_data
-    return {}
+    state_data = [data for data in india_data if state.lower() in data['state'].lower()]
+    if state_data:
+        district_data = [data for data in state_data[0]['districtData'] if district.lower() in data['district'].lower()]
+    return district_data
 
 
 if __name__ == '__main__':
