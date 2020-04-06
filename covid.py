e65973f852f9202ce59d7ce36edc1684610389d8
Md Hassan <mohammada@metroguild.com>
2020-04-06 18:01:34 +0530

    Cleaning code

--- covid.py
+++ covid.py
@@ -1,6 +1,7 @@
 import requests
 import json
 
+from collections import defaultdict
 from flask import Flask 
 from flask import request
 
@@ -17,12 +18,12 @@ def get_secret_message():
 def get_data():
     state = request.args.get('state', default='', type=str)
     district = request.args.get('district', default='', type=str)
-    district_data = []
     india_data = requests.get('https://api.covid19india.org/v2/state_district_wise.json').json()
     state_data = [data for data in india_data if state.lower() in data['state'].lower()]
-    if state_data:
-        district_data = [data for data in state_data[0]['districtData'] if district.lower() in data['district'].lower()]
-    return district_data
+    district_data = [data for data in state_data[0]['districtData'] if district.lower() in data['district'].lower()]
+    if district_data:
+        return district_data[0]
+    return {"Could not find data for the named state"}
 
 
 if __name__ == '__main__':
